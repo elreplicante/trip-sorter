c4e1a8b6dfb7c1ce29d9eefe63134de11e7ce9b1
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-17 22:36:51 +0100

    Adding tickets completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 TripSorter::Application.routes.draw do
-  # get "tickets/index"
-  # get "tickets/show"
-  # get "tickets/new"
-  # post "ticket", to: 'tickets#create', as: :create
-
   resources :tickets
+  root 'tickets#index'
 end
