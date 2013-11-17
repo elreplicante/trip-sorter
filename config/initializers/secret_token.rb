6452201b49619d6fd16b6339faa711fd22840ef5
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-17 20:46:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TripSorter::Application.config.secret_key_base = '84370b4f06bb5fdf81f622d3c3d89a57889c139b2ac1f8b2876bc1c9b433cbbba3304f1ddc22b3bfd65101f2c949869e4c3514f4d0d5dd2817003aa7fcff3182'
